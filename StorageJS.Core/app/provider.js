f9d176f4ed34514a4876e5a4d465c5a7412191d5
tenkiller <fincher.brett@gmail>
2014-01-14 23:12:14 -0500

    Initial upload

---
+++ StorageJS.Core/app/provider.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../vendor/jquery-2.0.3.min.js" />
+
+var storageJS = storageJS || {};
+
+(function (ns, $) {
+  function provider() {
+    this.keymap = {};
+  }
+
+  provider.prototype.register = function (identifier, options) {
+    if (identifier && typeof identifier === 'string') {
+      this.keymap[identifier] = options;
+    }
+  };
+
+  provider.prototype.request = function (identifier) {
+    return $.ajax(this.keymap[identifier]);
+  };
+
+  provider.prototype.remove = function (identifier) {
+    delete this.keymap[identifier];
+  };
+
+  ns.provider = provider;
+
+})(storageJS, jQuery);
---
+++ StorageJS.Core/app/storage.js
@@ -0,0 +1,41 @@
+﻿
+var storageJS = storageJS || {};
+
+(function (ns) {
+  var storage = function (type) {
+    this.store = (type === 'local') ? localStorage || {} : sessionStorage || {};
+  };
+
+  storage.prototype.getVal = function (key) {
+    return this.store[key];
+  };
+
+  storage.prototype.getObj = function (key) {
+    return JSON.parse(this.getVal(key));
+  };
+
+  storage.prototype.setVal = function (key, value) {
+    if (typeof value === 'object' || typeof value === 'function') return;
+    this.store[key] = value;
+  };
+
+  storage.prototype.setObj = function (key, value) {
+    if (typeof value === 'function') return;
+    this.setVal(key, JSON.stringify(value));
+  };
+
+  storage.prototype.length = function () {
+    return this.store.length;
+  };
+
+  storage.prototype.clear = function () {
+    if (this.store.constructor === Storage) {
+      this.store.clear();
+    } else {
+      this.store = {};
+    }
+  };
+
+  ns.storage = storage;
+
+})(storageJS);

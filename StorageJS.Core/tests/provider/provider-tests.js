4ea68c9bbc5a1044444d64c18645c8af192b0fdd
tenkiller <fincher.brett@gmail>
2014-01-18 15:13:19 -0500

    Added test framework for provider module

---
+++ StorageJS.Core/tests/provider/provider-tests.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../qunit-1.13.0.js" />
+/// <reference path="../../app/provider.js" />
+
+module('Provider Unit Tests');
+
+var provider = new storageJS.provider(),
+    getOptions = { url: '//localhost' };
+
+test('Initialization', function (assert) {
+  assert.strictEqual(typeof provider, 'object', 'Provider object instantiated.');
+});
+
+test('Register request', function (assert) {
+  provider.register('getData', getOptions);
+});
+
+test('Execute request', function (assert) {
+  
+});
+
+test('Remove request', function (assert) {
+  provider.remove('getData');
+});
